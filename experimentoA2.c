54e3369f0001ee12c961ef8db0e34adf231b00f9
lsavila <luca.avila@edu.ufes.br>
2022-11-25 12:18:38 -0300

    Juro que tentei fazer melhor que isso

--- experimentoA2.c
+++ experimentoA2.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 
-void calculaTempos(float* arrayDeTempo, float tempoMedio, float incertezaTempo, float tempoMedioQuadrado, float incertezaTempoQuadrado);
+void calculaTempos(float* arrayDeTempo, int opcao);
 int main(){
 
     float tempoSemPesoFinal[10] = {2.14280, 2.16565, 2.16330, 2.12995, 2.18985, 2.15090, 2.16955, 2.16385, 2.16675, 2.15490};
     float tempoComPesoFinal[10] = {2.51920, 2.50820, 2.52930, 2.51900, 2.53460, 2.53390, 2.52630, 2.52860, 2.55195, 2.51205};
-    float arrayValoresCalculadosSemPesoFinal[4] = {0, 0, 0, 0};
-    float arrayValoresCalculadosComPesoFinal[4] = {0, 0, 0, 0};
+
+    printf("Tempo médio sem peso final e incerteza: ");
+    calculaTempos(tempoSemPesoFinal, 1);
+    printf("Tempo médio ao quadrado sem peso e incerteza: ");
+    calculaTempos(tempoSemPesoFinal, 2);
+
+    printf("Tempo médio com peso e incerteza: ");
+    calculaTempos(tempoComPesoFinal, 1);
+    printf("Tempo médio ao quadrado com peso e incerteza: ");
+    calculaTempos(tempoComPesoFinal, 2);
+
     
-    calculaTempos(tempoSemPesoFinal, arrayValoresCalculadosSemPesoFinal[0], arrayValoresCalculadosSemPesoFinal[1], arrayValoresCalculadosSemPesoFinal[2], arrayValoresCalculadosSemPesoFinal[3]);
-    calculaTempos(tempoComPesoFinal, arrayValoresCalculadosComPesoFinal[0], arrayValoresCalculadosComPesoFinal[1], arrayValoresCalculadosComPesoFinal[2], arrayValoresCalculadosComPesoFinal[3]);
- 
-    printf("Tempo médio sem peso e incerteza: %.2f %.2f\n", arrayValoresCalculadosSemPesoFinal[0], arrayValoresCalculadosSemPesoFinal[1]);
-    printf("Tempo médio sem peso ao quadrado e incerteza: %.2f %.2f\n", arrayValoresCalculadosSemPesoFinal[2], arrayValoresCalculadosSemPesoFinal[3]);
-    printf("Tempo médio com peso e incerteza: %.2f %.2f\n", arrayValoresCalculadosComPesoFinal[0], arrayValoresCalculadosComPesoFinal[1]);
-    printf("Tempo médio com peso ao quadrado e incerteza: %.2f %.2f\n", arrayValoresCalculadosComPesoFinal[2], arrayValoresCalculadosComPesoFinal[3]);
-    return 0;
 }
 
-void calculaTempos(float* arrayDeTempo, float tempoMedio, float incertezaTempo, float tempoMedioQuadrado, float incertezaTempoQuadrado){
+void calculaTempos(float* arrayDeTempo, int opcao){
     // calculando tempo médio
     float tempoTotal = 0;
     float tempoTotalQuadrado = 0;
+    float tempoMedio = 0;
+    float tempoMedioQuadrado = 0;
+    float incertezaTempo = 0;
+    float incertezaTempoQuadrado = 0;
 
     for(int i = 0; i < 10; i++){
         tempoTotal += arrayDeTempo[i];
@@ -41,4 +46,14 @@ void calculaTempos(float* arrayDeTempo, float tempoMedio, float incertezaTempo, 
     } 
     incertezaTempo /= 10;
     incertezaTempoQuadrado /= 10;
+    
+    // deixei opcao como expoente do número que está sendo procurado
+    if(opcao == 1){
+        printf("%f %f\n", tempoMedio, incertezaTempo);
+    }
+
+    else if(opcao == 2){
+        printf("%f %f\n", tempoMedioQuadrado, incertezaTempoQuadrado);
+    }
+    
 }
