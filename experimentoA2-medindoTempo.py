9db225aa49eeb64db00f27fa0460362e931a3fb9
lsavila <luca.avila@edu.ufes.br>
2022-11-25 10:21:19 -0300

    Aqui estamos tratando só o tempo final desse inferno

--- experimentoA2-medindoTempo.py
+++ experimentoA2-medindoTempo.py
@@ -1,34 +1,44 @@
-tempoSemPeso = [2.14280, 2.16565, 2.16330, 2.12995, 2.18985, 2.15090, 2.16955, 2.16385, 2.16675, 2.15490]
-tempoComPeso = [2.51920, 2.50820, 2.52930, 2.51900, 2.53460, 2.53390, 2.52630, 2.52860, 2.55195, 2.51205]
+tempoSemPesoFinal = [2.14280, 2.16565, 2.16330, 2.12995, 2.18985, 2.15090, 2.16955, 2.16385, 2.16675, 2.15490]
+tempoComPesoFinal = [2.51920, 2.50820, 2.52930, 2.51900, 2.53460, 2.53390, 2.52630, 2.52860, 2.55195, 2.51205]
 
 # calculando tempo médio
-tempoTotalSemPeso = 0
+tempoTotalSemPesoFinal = 0
+tempoTotalSemPesoQuadradoFinal = 0
+tempoTotalComPesoFinal = 0
+tempoTotalComPesoQuadradoFinal = 0
+
 for i in range(10):
-    tempoTotalSemPeso += tempoSemPeso[i]
+    tempoTotalSemPesoFinal += tempoSemPesoFinal[i]
+    tempoTotalSemPesoQuadradoFinal += tempoSemPesoFinal[i]**2
+    tempoTotalComPesoFinal += tempoComPesoFinal[i]
+    tempoTotalComPesoQuadradoFinal += tempoComPesoFinal[i]**2
 
-tempoMedioSemPeso = tempoTotalSemPeso / 10
 
-tempoTotalComPeso = 0
-for i in range(10):
-    tempoTotalComPeso += tempoComPeso[i]
+tempoMedioSemPesoFinal = tempoTotalSemPesoFinal / 10
+tempoMedioSemPesoQuadrado = tempoTotalSemPesoQuadradoFinal / 10
+tempoMedioComPesoFinal = tempoTotalComPesoFinal / 10
+tempoMedioComPesoQuadrado = tempoTotalComPesoQuadradoFinal / 10
 
-tempoMedioComPeso = tempoTotalComPeso / 10
 
 # calculando as incertezas (incluindo operacao para ter apenas números positivos no somatório)
-incertezaTempoSemPeso = 0
-
+incertezaTempoSemPesoFinal = 0
+incertezaTempoSemPesoQuadradoFinal = 0
+incertezaTempoComPesoFinal = 0
+incertezaTempoComPesoQuadradoFinal = 0
 for i in range(10):
-    incertezaTempoSemPeso += tempoMedioSemPeso - tempoSemPeso[i] if tempoMedioSemPeso > tempoSemPeso[i] else tempoSemPeso[i] - tempoMedioSemPeso
+    incertezaTempoSemPesoFinal += tempoMedioSemPesoFinal - tempoSemPesoFinal[i] if tempoMedioSemPesoFinal > tempoSemPesoFinal[i] else tempoSemPesoFinal[i] - tempoMedioSemPesoFinal
+    incertezaTempoSemPesoQuadradoFinal += tempoMedioSemPesoQuadrado - tempoSemPesoFinal[i]**2 if tempoMedioSemPesoQuadrado > tempoSemPesoFinal[i]**2 else tempoSemPesoFinal[i]**2 - tempoMedioSemPesoQuadrado
+    incertezaTempoComPesoFinal += tempoMedioComPesoFinal - tempoComPesoFinal[i] if tempoMedioComPesoFinal > tempoComPesoFinal[i] else tempoComPesoFinal[i] - tempoMedioComPesoFinal
+    incertezaTempoComPesoQuadradoFinal += tempoMedioComPesoQuadrado - tempoComPesoFinal[i]**2 if tempoMedioComPesoQuadrado > tempoComPesoFinal[i]**2 else tempoComPesoFinal[i]**2 - tempoMedioComPesoQuadrado
     
-incertezaTempoSemPeso /= 10
 
-incertezaTempoComPeso = 0
+incertezaTempoSemPesoFinal /= 10
+incertezaTempoSemPesoQuadradoFinal /= 10
+incertezaTempoComPesoFinal /= 10
+incertezaTempoComPesoQuadradoFinal /= 10
 
-for i in range(10):
-    incertezaTempoComPeso += tempoMedioComPeso - tempoComPeso[i] if tempoMedioComPeso > tempoComPeso[i] else tempoComPeso[i] - tempoMedioComPeso
+print(f"Tempo médio sem peso e incerteza: {tempoMedioSemPesoFinal}  {incertezaTempoSemPesoFinal}")
+print(f"Tempo médio sem peso ao quadrado e incerteza: {tempoMedioSemPesoQuadrado} {incertezaTempoSemPesoQuadradoFinal}")
+print(f"Tempo médio com peso e incerteza: {tempoMedioComPesoFinal}  {incertezaTempoComPesoFinal}")
+print(f"Tempo médio com peso ao quadrado e incerteza: {tempoMedioComPesoQuadrado} {incertezaTempoComPesoQuadradoFinal}")
 
-incertezaTempoComPeso /= 10
-
-print(f"Tempo médio sem peso e incerteza: ${tempoMedioSemPeso}  ${incertezaTempoSemPeso} \n")
-
-print(f"Tempo médio com peso e incerteza: ${tempoMedioComPeso}  ${incertezaTempoComPeso} \n")
